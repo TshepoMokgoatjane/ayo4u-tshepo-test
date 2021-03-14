156c20a9e08a22d137b8bd19e42b9fe04c4b6ff1
Tshepo Mokgoatjane <F5103762@FNB.CO.ZA>
2021-03-15 00:31:40 +0200

    Enhancing Integration Tests

--- src/test/java/com/ayo4u/restful/webservice/conversion/ayo4urestfulwebserviceconversion/controller/ConversionControllerIntegrationTest.java
+++ src/test/java/com/ayo4u/restful/webservice/conversion/ayo4urestfulwebserviceconversion/controller/ConversionControllerIntegrationTest.java
@@ -5,6 +5,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
