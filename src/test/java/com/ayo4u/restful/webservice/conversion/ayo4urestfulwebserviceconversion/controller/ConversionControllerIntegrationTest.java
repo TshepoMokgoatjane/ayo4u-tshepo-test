478108d1f8a33bc2b25c77269124bfa15492f5b9
Tshepo Mokgoatjane <F5103762@FNB.CO.ZA>
2021-03-14 22:08:09 +0200

    Fixed Integration Test. Busy with all individual test

--- src/test/java/com/ayo4u/restful/webservice/conversion/ayo4urestfulwebserviceconversion/controller/ConversionControllerIntegrationTest.java
+++ src/test/java/com/ayo4u/restful/webservice/conversion/ayo4urestfulwebserviceconversion/controller/ConversionControllerIntegrationTest.java
@@ -1,7 +1,8 @@
 package com.ayo4u.restful.webservice.conversion.ayo4urestfulwebserviceconversion.controller;
 
 import static org.mockito.ArgumentMatchers.any;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -10,21 +11,22 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.ayo4u.restful.webservice.conversion.ayo4urestfulwebserviceconversion.model.ConversionModel;
 import com.ayo4u.restful.webservice.conversion.ayo4urestfulwebserviceconversion.service.ConversionService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith(MockitoExtension.class)
 public class ConversionControllerIntegrationTest {
 
-	/*@Mock
+	@Mock
 	private ConversionService conversionService;
 	
 	@InjectMocks
 	private ConversionController conversionController;
-	
 	
 	private MockMvc mockMvc;
 	
@@ -33,11 +35,22 @@ public class ConversionControllerIntegrationTest {
 		mockMvc = MockMvcBuilders.standaloneSetup(conversionController).build();
 	}
 	
-	/*@Test
-	void testConvertMetrics() {
-		ObjectMapper objectMapper = new ObjectMapper();
-		Mockito.when(conversionService.doConversion(any(ConversionModel.class))).thenReturn(500.25);
+	@Test
+	void testConvertMetrics() throws Exception {
 		
-		//mockMvc.perform(post());
-	}*/
+		ObjectMapper objectMapper = new ObjectMapper();
+		
+		Mockito.when(conversionService.doConversion(any(ConversionModel.class)))
+				.thenReturn(550.25);
+		
+		mockMvc.perform(post("/api/v1/convert-metric")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(getRequest())))
+	            .andExpect(status().isOk())
+	            .andReturn();
+	}
+	            
+	private ConversionModel getRequest() {
+		return new ConversionModel("METER", "KILOMETER",500); 
+	}
 }
